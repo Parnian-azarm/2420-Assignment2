618c373ab8b0ee61ac75defcd9db555a7c143a85
Parnian-azarm <pariazarm5@gmail.com>
2022-12-02 17:09:08 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'Server one' }
+	
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
